2ac433b9c362c7a71b574275cd153bf2ad63b191
gregl83 <general@gregorylanglais.com>
2021-12-17 18:13:17 -0500

    add sorted paths to merkle tree
    - use btreemap for sorted paths (guarantee same results each run)
    - handle directory path changes by adding paths to merkle tree

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,40 @@
 use std::fs;
 use std::path::Path;
+use std::collections::BTreeMap;
 use walkdir::WalkDir;
-use rs_merkle::{algorithms::Sha256, Error, Hasher, MerkleTree};
+use rs_merkle::{algorithms::Sha256, Hasher, MerkleTree};
 
-fn get_file_paths(root: &str) -> Vec<String> {
-    let mut file_paths = Vec::<String>::new();
+fn get_paths(root: &str) -> BTreeMap<String, bool> {
+    // BTreeMap ensures order of files/directories by path
+    let mut paths = BTreeMap::<String, bool>::new();
+
     for entry in WalkDir::new(root)
             .follow_links(true)
             .into_iter()
             .filter_map(|e| e.ok()) {
-        if entry.path().is_file() {
-            let canonical_file_path = fs::canonicalize(entry.path()).unwrap();
-            file_paths.push(String::from(canonical_file_path.as_os_str().to_str().unwrap()));
-        }
+        let path = String::from(entry.path().as_os_str().to_str().unwrap());
+        paths.insert(path, entry.path().is_file());
     }
-    file_paths
+    paths
 }
 
-fn hash_file_paths(file_paths: Vec<String>) -> Vec<[u8; 32]> {
+fn hash_paths(paths: BTreeMap<String, bool>) -> Vec<[u8; 32]> {
     let mut hashes = Vec::<[u8; 32]>::new();
 
-    for file_path in file_paths {
-        let file_bytes = fs::read(file_path).unwrap();
-        hashes.push(Sha256::hash(file_bytes.as_slice()))
+    for (absolute_path, is_file) in paths {
+        // hash paths for fs changes other than file content
+        hashes.push(Sha256::hash(absolute_path.as_bytes()));
+        // for files add hash of contents
+        if is_file {
+            let file_bytes = fs::read(absolute_path).unwrap();
+            hashes.push(Sha256::hash(file_bytes.as_slice()))
+        }
     }
 
     hashes
 }
 
-fn get_file_hashes_root(file_hashes: Vec<[u8; 32]>) -> Option<String> {
+fn get_hashes_root(file_hashes: Vec<[u8; 32]>) -> Option<String> {
     MerkleTree::<Sha256>::from_leaves(&file_hashes).root_hex()
 }
 
@@ -36,8 +42,8 @@ fn main() {
     // todo - add clap with dir argument
     // todo - handle single file and multiple files (dir path)
     // todo - add error handling with messaging
-    let file_paths = get_file_paths(".");
-    let file_hashes = hash_file_paths(file_paths);
-    let root = get_file_hashes_root(file_hashes).unwrap();
+    let file_paths = get_paths(".");
+    let file_hashes = hash_paths(file_paths);
+    let root = get_hashes_root(file_hashes).unwrap();
     println!("{}", root.as_str());
 }
