b261d9c026836a48713ff0aab9b90d3172c4ee1c
gregl83 <general@gregorylanglais.com>
2021-12-17 20:13:33 -0500

    bump v0.3.2

--- src/main.rs
+++ src/main.rs
@@ -48,7 +48,7 @@ fn main() {
     // todo - add error handling with messaging
 
     let matches = App::new("paq")
-        .version("0.3.1")
+        .version("0.3.2")
         .about("paq files to hash.")
         .arg(Arg::with_name("src")
             .help("Source to hash (path)")
