1d5438ff9aae0871b446719c038fb12b69dd99cf
gregl83 <general@gregorylanglais.com>
2022-03-19 14:32:50 -0400

    add single file hash test

--- tests/functional.rs
+++ tests/functional.rs
@@ -7,10 +7,10 @@ mod utils;
 use utils::TempDir;
 
 #[test]
-fn it_hashes_empty_directory() {
+fn it_hashes_directory() {
     let expectation = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
 
-    let dir = TempDir::new("it_hashes_empty_directory").unwrap();
+    let dir = TempDir::new("it_hashes_directory").unwrap();
     let source = dir.path().as_os_str().to_str().unwrap();
 
     let hash_ignored = paq::hash_source(source, true);
@@ -20,10 +20,10 @@ fn it_hashes_empty_directory() {
 }
 
 #[test]
-fn it_hashes_from_any_path() {
+fn it_hashes_directory_from_any_path() {
     let expectation = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
 
-    let dir = TempDir::new("it_hashes_from_any_path").unwrap();
+    let dir = TempDir::new("it_hashes_directory_from_any_path").unwrap();
     let source = dir.path().as_os_str().to_str().unwrap();
     let original_path = env::current_dir().unwrap();
     let new_path = PathBuf::from("/");
@@ -44,12 +44,12 @@ fn it_hashes_from_any_path() {
 }
 
 #[test]
-fn it_hashes_symlinks_without_following() {
+fn it_hashes_directory_symlink_without_following() {
     let expectation = "778e05e084f80adb22043d7e45ad7236e1fe07bfb9983f5f26867c205dcc0112";
 
     let symlink_name = "symlink";
     let symlink_target = env::current_dir().unwrap();
-    let dir = TempDir::new("it_hashes_symlinks_without_following").unwrap();
+    let dir = TempDir::new("it_hashes_directory_symlink_without_following").unwrap();
     dir.new_symlink(symlink_name, symlink_target).unwrap();
     let source = dir.path().as_os_str().to_str().unwrap();
 
@@ -57,4 +57,21 @@ fn it_hashes_symlinks_without_following() {
     assert_eq!(hash_ignored, expectation);
     let hash_ignored = paq::hash_source(source, false);
     assert_eq!(hash_ignored, expectation);
+}
+
+#[test]
+fn it_hashes_single_file() {
+    let expectation = "357dc7acd9dd411e9bd92945cad58062559910dbc229a7f86a21687e626a955e";
+
+    let file_name = "alpha";
+    let file_contents = "alpha-body".as_bytes();
+    let dir = TempDir::new("it_hashes_single_file").unwrap();
+    dir.new_file(file_name, file_contents).unwrap();
+    let file_directory = dir.path().join(file_name);
+    let source = file_directory.as_os_str().to_str().unwrap();
+
+    let hash_ignored = paq::hash_source(source, true);
+    assert_eq!(hash_ignored, expectation);
+    let hash_not_ignored = paq::hash_source(source, false);
+    assert_eq!(hash_not_ignored, expectation);
 }
