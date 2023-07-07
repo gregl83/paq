0161d3e6b954ab566c2f6a585ea52b859c4ded32
gregory langlais <admin@gregorylanglais.com>
2023-07-07 15:28:08 -0500

    update gh workflow for windows target error test

--- src/lib.rs
+++ src/lib.rs
@@ -74,6 +74,11 @@ fn hash_paths(root: &PathBuf, paths: Vec<PathBuf>) -> Vec<[u8; 32]> {
             {
                 hasher.update(symlink_target.to_str().unwrap().replace("\\", "/").as_bytes());
             }
+            println!(
+                "PATH: {:?}  CONTENT: {:?}",
+                source_path,
+                symlink_target
+            );
         } else if metadata.is_file() {
             // for files add hash of contents
             let mut file = fs::File::open(relative_path).unwrap();
@@ -81,6 +86,11 @@ fn hash_paths(root: &PathBuf, paths: Vec<PathBuf>) -> Vec<[u8; 32]> {
             loop {
                 let buffer_size = file.read(&mut buffer[..]).unwrap();
                 if buffer_size == 0 { break }
+                println!(
+                    "PATH: {:?}  CONTENT: {:?}",
+                    source_path,
+                    String::from_utf8(buffer[..buffer_size].to_vec()).unwrap()
+                );
                 hasher.update(&buffer[..buffer_size]);
             }
         }
