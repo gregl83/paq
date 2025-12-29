e6f36a7cc1c0d5c7afd65d41d148c371c2be10c1
gregory langlais <_@gregorylanglais.com>
2025-12-29 13:31:23 -0800

    refactor for jwalk implementation
    - slight performance edge over walkdir with larger dirs

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,18 @@
 use std::{
     fs,
     io::prelude::*,
+    iter,
     path::Path,
 };
 
 pub use arrayvec::ArrayString;
 use blake3::Hasher;
-use memmap2::Mmap;
-use rayon::prelude::*;
-use walkdir::{
+use jwalk::{
     DirEntry,
     WalkDir,
 };
+use memmap2::Mmap;
+use rayon::prelude::*;
 
 
 pub const PATH_BATCH_SIZE: usize = 100;
@@ -25,25 +26,7 @@ pub const FILE_BUFFER_SIZE: usize = 32 * 1024;
 #[cfg(target_os = "windows")]
 pub const FILE_BUFFER_SIZE: usize = 128 * 1024;
 
-#[inline]
-fn is_hidden(entry: &DirEntry) -> bool {
-    entry
-        .file_name()
-        .to_str()
-        .map(|s| s != "." && s.starts_with("."))
-        .unwrap_or(false)
-}
-
-#[inline]
-fn filter(ignore_hidden: bool) -> impl FnMut(&DirEntry) -> bool {
-    if ignore_hidden {
-        |entry: &DirEntry| -> bool { !is_hidden(entry) }
-    } else {
-        |_: &DirEntry| -> bool { true }
-    }
-}
-
-fn buffer_file_to_hasher(hasher: &mut Hasher, path: &str) {
+fn buffer_file_to_hasher(hasher: &mut Hasher, path: &Path) {
     let mut file = fs::File::open(path).unwrap();
     let mut buffer = [0; FILE_BUFFER_SIZE];
     loop {
@@ -53,10 +36,12 @@ fn buffer_file_to_hasher(hasher: &mut Hasher, path: &str) {
     }
 }
 
-fn hash_path(root: &Path, entry: &DirEntry) -> [u8; 32] {
-    let mut hasher = Hasher::new();
+fn hash_path(root: &Path, entry: &DirEntry<((), ())>) -> [u8; 32] {
     let path = entry.path();
     let source_path = path.strip_prefix(root).unwrap().to_str().unwrap();
+    let source_type = entry.file_type();
+
+    let mut hasher = Hasher::new();
     // hash paths for fs changes other than file content (must be relative to root)
     #[cfg(target_family = "unix")]
     {
@@ -66,11 +51,9 @@ fn hash_path(root: &Path, entry: &DirEntry) -> [u8; 32] {
     {
         hasher.update(source_path.replace("\\", "/").as_bytes());
     }
-    let relative_path = path.as_os_str().to_str().unwrap();
-    let metadata = entry.metadata().unwrap();
-    if metadata.is_symlink() {
+    if source_type.is_symlink() {
         // for symlinks add hash of target path
-        let symlink_target = fs::read_link(relative_path).unwrap();
+        let symlink_target = fs::read_link(&path).unwrap();
         #[cfg(target_family = "unix")]
         {
             hasher.update(symlink_target.to_str().unwrap().as_bytes());
@@ -85,8 +68,9 @@ fn hash_path(root: &Path, entry: &DirEntry) -> [u8; 32] {
                     .as_bytes(),
             );
         }
-    } else if metadata.is_file() {
+    } else if source_type.is_file() {
         // for files, add contents to hasher
+        let metadata = entry.metadata().unwrap();
         let file_size = metadata.len();
         if file_size == 0 {
             // empty file, return immediately
@@ -97,14 +81,14 @@ fn hash_path(root: &Path, entry: &DirEntry) -> [u8; 32] {
             hasher.update(&file);
         } else if file_size > MIN_FILE_SIZE_FOR_MMAP_READ {
             // large size files read using mmap or fail to buffered read
-            let file = fs::File::open(path).unwrap();
+            let file = fs::File::open(&path).unwrap();
             match unsafe { Mmap::map(&file) } {
                 Ok(mmap) => { hasher.update(&mmap); },
-                Err(_) => { buffer_file_to_hasher(&mut hasher, relative_path); },
+                Err(_) => { buffer_file_to_hasher(&mut hasher, &path) ; },
             }
         } else {
             // medium file size read using buffer
-            buffer_file_to_hasher(&mut hasher, relative_path);
+            buffer_file_to_hasher(&mut hasher, &path);
         }
     }
     *hasher.finalize().as_bytes()
@@ -136,17 +120,17 @@ fn get_hashes_root(file_hashes: Vec<[u8; 32]>) -> ArrayString<64> {
 /// assert_eq!(&source_hash[..], "d7d25c9b2fdb7391e650085a985ad0d892c7f0dd5edd32c7ccdb4b0d1c34c430");
 /// ```
 pub fn hash_source(source: &Path, ignore_hidden: bool) -> ArrayString<64> {
-    // construct file system walker
-    let mut walker = WalkDir::new(source)
-        .follow_links(false)
-        .into_iter()
-        .filter_entry(filter(ignore_hidden));
+    // construct parallel file system walker (unordered)
+    let walker = WalkDir::new(source)
+        .skip_hidden(ignore_hidden)
+        .follow_links(false);
 
     // construct iterator that retrieves system path batches using walker
-    let batch_iter = std::iter::from_fn(move || {
+    let mut walker_iter = walker.into_iter();
+    let batch_iter = iter::from_fn(move || {
         let mut batch = Vec::with_capacity(PATH_BATCH_SIZE);
         for _ in 0..PATH_BATCH_SIZE {
-            match walker.next() {
+            match walker_iter.next() {
                 Some(Ok(entry)) => batch.push(entry),
                 Some(Err(e)) => panic!("Critical: Failed to traverse directory: {e}"),
                 None => break,
@@ -159,9 +143,8 @@ pub fn hash_source(source: &Path, ignore_hidden: bool) -> ArrayString<64> {
     let mut hashes: Vec<[u8; 32]> = batch_iter
         .par_bridge()
         .flat_map_iter(|batch| {
-            batch.into_iter().map(|entry| {
-                hash_path(source, &entry)
-            })
+            // process the whole batch (low lock contention)
+            batch.into_iter().map(|entry| hash_path(source, &entry))
         })
         .collect();
 
